79b89fe257a20c6285fbae403218a450578d00bb
sixeyes198 <yossiguadie@icloud.com>
2024-02-22 13:40:08 +0200

    Update

--- Server.js
+++ Server.js
@@ -1,13 +1,22 @@
-import express, { urlencoded } from "express";
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.get("/api", (req, res) => {
-  res.status(200).send("<h1>Test</h1>");
+// css static link
+app.use(express.static(path.join(__dirname)));
+// Home page
+app.get("/Homepage", (req, res) => {
+  res.status(200).sendFile(__dirname + "/index.html");
 });
+
+// console.log(__dirname);
 
 app.listen(process.env.Port || 3000, (req, res) => {
   console.log(`Server running on ${process.env.Port || 3000}`);

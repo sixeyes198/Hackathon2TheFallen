f78367a551963820258aa08607540562912a7788
sixeyes198 <yossiguadie@icloud.com>
2024-02-24 21:34:08 +0200

    update

--- public/Script.js
+++ public/Script.js
@@ -1,13 +1,23 @@
-const data = document.getElementById("fallenData");
+const dataContainer = document.getElementById("fallenData");
 
-const result = async () => {
+const fetchData = async () => {
   try {
     const res = await fetch("http://localhost:3000/fallen");
     console.log(res);
     const data = await res.json();
     console.log(data);
+    dataContainer.innerHTML = "";
+
+    data.forEach((item) => {
+      dataContainer.innerHTML += ` <div class="result">
+     <h2>${item.first_name}</h2>
+     <h3>${item.last_name}</h3>
+     <p>${item.age}</p>
+     <p>${item.content}</p>
+    </div>`;
+    });
   } catch (error) {
     console.log(error);
   }
 };
-result();
+fetchData();
